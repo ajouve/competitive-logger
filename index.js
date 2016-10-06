f74af24297f576978a4fdaa8516559eaacc1268d
ajouve <jouve.antoine.27@gmail.com>
2016-10-06 11:24:44 +0200

    Add and remove transport

--- index.js
+++ index.js
@@ -5,33 +5,43 @@ var logLevels = ['error', 'warn', 'info', 'debug'];
 var transformers = [];
 
 var logger = {
-  configureRedis: function(config) {
-    winstonRedis.Redis;
-    winston.add(winston.transports.Redis, {
-      host: config.host,
-      auth: config.password
-    });
-  },
-  addTransformer: function(transformer) {
-    transformers.push(transformer);
-  },
-  disable: function() {
-    winston.clear();
-  }
+    configureRedis: function(config) {
+        winstonRedis.Redis;
+        winston.add(winston.transports.Redis, {
+            host: config.host,
+            auth: config.password
+        });
+    },
+
+    addTransformer: function(transformer) {
+        transformers.push(transformer);
+    },
+
+    disable: function() {
+        winston.clear();
+    },
+
+    removeTransport(type) {
+        winston.remove(winston.transports[type]);
+    }
+
+    addTransport(type, options) {
+        winston.add(winston.transports[type], options);
+    }
 };
 
-logLevels.forEach(function(logLevel){
-  logger[logLevel] = function(message, meta) {
-    if (!meta) meta = {};
-    transform(message, meta);
-    winston[logLevel](message, meta)
-  }
+logLevels.forEach(function(logLevel) {
+    logger[logLevel] = function(message, meta) {
+        if (!meta) meta = {};
+        transform(message, meta);
+        winston[logLevel](message, meta)
+    }
 });
 
 function transform(message, meta) {
-  transformers.forEach(function(transformer){
-    transformer(message, meta);
-  });
+    transformers.forEach(function(transformer) {
+        transformer(message, meta);
+    });
 }
 
 module.exports = logger;
